ecacb1ad7930f149520c6e7947bf1e628a0d66e6
Jatin Saini <jatinrohitsaini@gmail.com>
2025-07-18 23:44:30 +0530

    Routing and Request Handlers

--- src/app.js
+++ src/app.js
@@ -2,14 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/jatin", (req, res) => {
-    res.send("hello from the jatin")
-})
-app.use((req, res) => {
-    res.send("hello from the server")
-})
+app.post("/jatin", (req, res) => {
+  res.send({firstName:"jatin", lastname:"saini"});
+});
 
-app.listen(3000, ()=>{
-    console.log("hey birather");
-    
-})
+app.get("/jatin", (req, res) => {
+  res.send("hello from the jatin");
+});
+
+// use handles all types in same route so we have use different types 
+app.use("/jatin", (req, res) => {
+  res.send("hello from the jatin");
+});
+
+// app.use("/server", (req, res) => {
+//   res.send("hello from the server");
+// });
+
+// app.use("/server/2", (req, res) => {
+//   res.send("hello from server 2");
+// });
+
+app.listen(3000, () => {
+  console.log("hey birather");
+});
