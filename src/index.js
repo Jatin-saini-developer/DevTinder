3301ac9de525eb33305e2f9ee92484f85079abb1
Jatin Saini <jatinrohitsaini@gmail.com>
2025-07-22 23:30:58 +0530

    API

--- src/index.js
+++ src/index.js
@@ -4,19 +4,59 @@ const connectDB = require("./config/database");
 const User = require("./model/User")
 const app = express();
 
+app.use(express.json());
 
+// POST API
 app.post("/signUp", async (req, res, next)=>{
-    const ojb = new User({
-        firstName : "anisha",
-        lastName : "Saini",
-        password : "anisha@123",
-        email : "anisha@gmail.com"
-    })
+
+    console.log(req.body)
+    const ojb = new User(req.body
+        // firstName : "anisha",
+        // lastName : "Saini",
+        // password : "anisha@123",
+        // email : "anisha@gmail.com"
+
+    )
 
     await ojb.save()
     res.send("user added successfully")
-})
 
+
+});
+
+
+// GET API
+app.get("/user", async (req, res)=>{
+    const userEmail = req.body.email;
+
+    try{
+     const user = await User.find({email : userEmail});
+     res.send(user);
+    }
+    catch{
+        console.log("Error 69 ");
+        
+    }
+
+});
+
+
+// DELETE API
+app.delete("/user", async (req, res)=>{
+
+    const userId = req.body.userId
+
+    try{
+    const user = await User.findByIdAndDelete(userId)
+    res.send('user deleted successfully')
+    }
+    catch{
+        console.log("Error 69");
+        
+    }
+
+
+})
 
 connectDB()
 .then(()=>{
