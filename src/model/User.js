f3e773c6b89b7e264784fbd7efcc2bac2c988496
Jatin Saini <jatinrohitsaini@gmail.com>
2025-07-27 13:38:52 +0530

    schema validations

--- src/model/User.js
+++ src/model/User.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true
     },
     lastName : {
         type : String
     },
     email : {
-        type : String
+        type : String,
+        required : true,
+        unique : true
     },
     password : {
         type : String
